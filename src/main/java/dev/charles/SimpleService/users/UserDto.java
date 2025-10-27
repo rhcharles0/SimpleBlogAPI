41c138edcb8c1ee7df949da31a480f2dceb6a209
Charles <rhcharles0@gmail.com>
2025-10-27 20:56:14 +0900

    fix(dto): Correct regexp validation

--- src/main/java/dev/charles/SimpleService/users/UserDto.java
+++ src/main/java/dev/charles/SimpleService/users/UserDto.java
@@ -9,13 +9,13 @@ import org.hibernate.validator.constraints.Length;
 
 public record UserDto(
         @Length(min = 5, max = 15, message = "5이상 15이하 글자를 입력하세요.")
-        @Pattern(regexp = "^[a-zA-Z가-힣]{5,}$", message = "올바른 이름을 입력하세요.")
+        @Pattern(regexp = "^[a-zA-Z가-힣][a-zA-Z가-힣0-9]{4,}$", message = "올바른 이름을 입력하세요.")
         @NotBlank(message = "사용자 이름은 필수입니다.")
         @JsonProperty
         String username,
 
         @Email(
-                regexp = "^[a-zA-Z0-9_가-힣]{5,}@[a-zA-Z0-9_가-힣]+\\.[a-z]{2,}$",
+                regexp = "^[a-zA-Z][a-zA-Z0-9]{4,}@[a-zA-Z]+\\.[a-z]{2,}$",
                 message = "올바른 이메일을 입력하세요."
         )
         @NotBlank(message = "이메일은 필수입니다.")
