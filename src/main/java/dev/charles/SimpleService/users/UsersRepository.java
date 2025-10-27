a04d6ee6b3405fee11cf33069928d7f47bafbb56
Charles <rhcharles0@gmail.com>
2025-10-27 20:55:17 +0900

    fix(repository): Correct method signature for paged user retrieval

--- src/main/java/dev/charles/SimpleService/users/UsersRepository.java
+++ src/main/java/dev/charles/SimpleService/users/UsersRepository.java
@@ -3,17 +3,11 @@ package dev.charles.SimpleService.users;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
 import java.util.Optional;
 
 interface UsersRepository extends JpaRepository<Users, Long> {
-
     Optional<Users> findByEmail(String email);
-
-
     <T> Optional<T> findByEmail(String email, Class<T> type);
     <T> Optional<T> findById(Long id, Class<T> type);
-
-    <T> Page<T> findAllBy(Pageable pageable, Class<T> type);
+    Page<UserDto> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
