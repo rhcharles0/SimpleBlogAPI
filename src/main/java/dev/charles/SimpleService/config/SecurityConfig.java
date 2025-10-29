caf22c3de90fff3cc077dd056af500983b9a38b3
Charles <rhcharles0@gmail.com>
2025-10-29 16:51:53 +0900

    feat(config): add `SecurityConfig`

--- src/main/java/dev/charles/SimpleService/config/SecurityConfig.java
+++ src/main/java/dev/charles/SimpleService/config/SecurityConfig.java
@@ -1,9 +1,44 @@
 package dev.charles.SimpleService.config;
 
+import dev.charles.SimpleService.security.introspector.GoogleOpaqueTokenIntrospector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain (HttpSecurity http, OpaqueTokenIntrospector introspector) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorize-> authorize
+                                .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
+                                .anyRequest().authenticated()
+                ).oauth2ResourceServer(oauth2 -> oauth2
+                        .opaqueToken(opaqueToken -> opaqueToken
+                                .introspector(introspector)
+                        )
+                );
+        return http.build();
+    }
 
+    @Bean
+    public OpaqueTokenIntrospector introspector(RestTemplateBuilder builder, OAuth2ResourceServerProperties properties) {
+        RestOperations rest = builder
+                .build();
 
+        return new GoogleOpaqueTokenIntrospector(properties.getOpaquetoken().getIntrospectionUri(), rest);
+    }
 }
