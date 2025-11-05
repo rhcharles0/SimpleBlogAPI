5899a4bf4a4031b3dcea894dab5dfee7825e0f24
Charles <rhcharles0@gmail.com>
2025-11-05 17:22:35 +0900

    perf(inspector): set connectTimeout and readTimeout to improve performance

--- src/main/java/dev/charles/SimpleService/config/SecurityConfig.java
+++ src/main/java/dev/charles/SimpleService/config/SecurityConfig.java
@@ -1,42 +1,61 @@
 package dev.charles.SimpleService.config;
 
-import dev.charles.SimpleService.security.introspector.GoogleOpaqueTokenIntrospector;
-import org.springframework.beans.factory.annotation.Value;
+import dev.charles.SimpleService.utils.security.introspector.GoogleOpaqueTokenIntrospector;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
-import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandlerImpl;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
 import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+//    @Bean
+//    AuthenticationEntryPoint authenticationEntryPoint = new BearerTokenAuthenticationEntryPoint();
+
     @Bean
     SecurityFilterChain filterChain (HttpSecurity http, OpaqueTokenIntrospector introspector) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorize-> authorize
-                                .requestMatchers(HttpMethod.GET, "/api/users/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/users","/api/users/**").permitAll()
                                 .anyRequest().authenticated()
                 ).oauth2ResourceServer(oauth2 -> oauth2
                         .opaqueToken(opaqueToken -> opaqueToken
-                                .introspector(introspector)
-                        )
+                                .introspector(introspector))
+
                 );
+
+        http
+                .exceptionHandling(handling -> handling
+                        .accessDeniedHandler(new AccessDeniedHandlerImpl()));
+
         return http.build();
     }
 
     @Bean
     public OpaqueTokenIntrospector introspector(RestTemplateBuilder builder, OAuth2ResourceServerProperties properties) {
         RestOperations rest = builder
+                .connectTimeout(Duration.of(30, ChronoUnit.SECONDS))
+                .readTimeout(Duration.of(30, ChronoUnit.SECONDS))
                 .build();
 
         return new GoogleOpaqueTokenIntrospector(properties.getOpaquetoken().getIntrospectionUri(), rest);
